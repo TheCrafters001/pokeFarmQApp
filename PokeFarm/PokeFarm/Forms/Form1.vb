dafe47ad225975db580d6939d6a2df7f62f8ca9d
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2023-10-02 02:45:59 -0400

    Woops

--- PokeFarm/PokeFarm/Forms/Form1.vb
+++ PokeFarm/PokeFarm/Forms/Form1.vb
@@ -6,6 +6,9 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
+
+            Timer1.Start()
+
             nav.updateStatus("")
 
             startup.preInit()
